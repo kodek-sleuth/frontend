27dc04cdfc23b423493b17f9c4b6d6a6bca0a20d
Mugerwa Joseph <josekodek2000@gmail.com>
2019-03-10 17:57:51 +0300

    .

--- Js/home.js
+++ Js/home.js
@@ -1,83 +1,3 @@
-function hideRest()
-{
-    document.getElementById('secondOne').style.display='none';
-    document.getElementById('thirdOne').style.display='none';
-
-}
-hideRest()
-
-document.getElementsByClassName('Icright')[0].addEventListener('click', showCafe);
-function showCafe()
-{
-    document.getElementById('firstOne').style.display='none';
-    $(function()
-    {
-        $('#secondOne').show()
-    }
-)
-
-}
-
-document.getElementsByClassName('Icright')[1].addEventListener('click', showPizza);
-function showPizza()
-{
-    document.getElementById('secondOne').style.display='none';
-    $(function()
-    {
-        $('#thirdOne').show()
-    }
-)
-
-}
-
-document.getElementsByClassName('Icright')[2].addEventListener('click', showKfc);
-function showKfc()
-{
-    document.getElementById('thirdOne').style.display='none';
-    $(function()
-    {
-        $('#firstOne').show()
-    }
-)
-
-}
-
-document.getElementsByClassName('restIcons')[0].addEventListener('click', showCafe);
-function showCafe()
-{
-    document.getElementById('firstOne').style.display='none';
-    $(function()
-    {
-        $('#secondOne').show()
-    }
-)
-
-}
-
-document.getElementsByClassName('restIcons')[1].addEventListener('click', showPizza);
-function showPizza()
-{
-    document.getElementById('secondOne').style.display='none';
-    $(function()
-    {
-        $('#thirdOne').show()
-    }
-)
-
-}
-
-document.getElementsByClassName('restIcons')[2].addEventListener('click', showKfc);
-function showKfc()
-{
-    document.getElementById('thirdOne').style.display='none';
-    $(function()
-    {
-        $('#firstOne').show()
-    }
-)
-
-}
-
 //MAKING ORDERS AND ADDING TO CART
 document.getElementsByClassName('cartBtns')[0].addEventListener('click', addToCart);
 function addToCart()
@@ -172,24 +92,65 @@ function toCart()
 }
 
 //After Clicking SlideShow
-document.getElementsByClassName('toPage')[0].addEventListener('click', toKfc);
+document.getElementsByClassName('sdImg')[0].addEventListener('click', toKfc);
 function toKfc()
 {
     window.location='/phoneKfc.html';
 }
 
-document.getElementsByClassName('toPage')[1].addEventListener('click', toJavas);
+document.getElementsByClassName('sdImg')[1].addEventListener('click', toJavas);
 function toJavas()
 {
     window.location='/phoneJavas.html';
 }
 
-document.getElementsByClassName('toPage')[0].addEventListener('click', toPizza);
+document.getElementsByClassName('sdImg')[2].addEventListener('click', toPizza);
 function toPizza()
 {
     window.location='/phonePizza.html';
 }
 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n)
+{
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if(n > slides.length)
+   {
+       slideIndex = 1
+   } 
+
+  if(n < 1)
+   {
+       slideIndex = slides.length
+   }
+  
+   for(i = 0; i < slides.length; i++)
+    {
+      slides[i].style.display = "none"; 
+    }
+
+   for(i = 0; i < dots.length; i++)
+    {
+      dots[i].className = dots[i].className.replace("active", " ");
+    }
+  
+    slides[slideIndex-1].style.display = "block"; 
+    dots[slideIndex-1].className += " active";
+}
 
 
 
